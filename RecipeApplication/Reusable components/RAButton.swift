8abcfa6e912a54c2b02c3cf02ef08eef41094463
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-18 23:06:06 +0400

    add button

---
+++ RecipeApplication/Reusable components/RAButton.swift
@@ -0,0 +1,35 @@
+//
+//  RAButton.swift
+//  RecipeApplication
+//
+//  Created by Lika Nozadze on 1/18/24.
+//
+
+import UIKit
+
+class RAButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+        
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
