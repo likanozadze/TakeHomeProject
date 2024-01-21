53fed9e6f02582b6931205be23b40e1070fc4fc6
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-21 16:18:56 +0400

    create register and login models, authservice for firebase

---
+++ RecipeApplication/Models/LoginUserRequest.swift
@@ -0,0 +1,13 @@
+//
+//  LoginUserRequest.swift
+//  RecipeApplication
+//
+//  Created by Lika Nozadze on 1/21/24.
+//
+
+import Foundation
+
+struct LoginUserRequest {
+    let email: String
+    let password: String
+}
---
+++ RecipeApplication/Models/RegisterUserRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterUserRequest.swift
+//  RecipeApplication
+//
+//  Created by Lika Nozadze on 1/21/24.
+//
+
+import Foundation
+
+struct RegisterUserRequest {
+    let username: String
+    let email: String
+    let password: String
+}
