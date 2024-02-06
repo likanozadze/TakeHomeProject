50f7981ecfe7c49b9f60fd9dd50ae0f47af34b6f
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-02-07 03:01:23 +0400

    modify dummyRecipeView

--- RecipeApplication/Scenes/Generate/DummyRecipeView.swift
+++ RecipeApplication/Scenes/Generate/DummyRecipeView.swift
@@ -21,12 +21,12 @@ struct DummyRecipeView: View {
                     Image(recipe.image)
                         .resizable()
                         .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .frame(height: 180)
+                        .frame(height: 260)
                     
                     
                     HStack {
                         Text(recipe.title)
-                            .font(.system(size: 18))
+                            .font(.system(size: 20))
                             .font(.headline)
                             .fontWeight(.bold)
                             .foregroundColor(.primary)
@@ -34,11 +34,11 @@ struct DummyRecipeView: View {
                         Spacer()
                         HStack {
                             Image(systemName: "clock")
-                                .font(.system(size: 14))
+                                .font(.system(size: 16))
                                 .foregroundColor(.primary)
                             
                             Text("\(recipe.cookTime) Min")
-                                .font(.system(size: 14))
+                                .font(.system(size: 16))
                                 .font(.headline)
                                 .foregroundColor(.primary)
                         }
@@ -52,10 +52,7 @@ struct DummyRecipeView: View {
                         .font(.headline)
                         .fontWeight(.bold)
                         .font(.system(size: 16))
-                        .foregroundColor(.white)
-                        .frame(width: 120, height: 30)
-                        .background(Color(red: 134/255, green: 191/255, blue: 62/255, opacity: 1.0))
-                        .cornerRadius(8)
+                        .foregroundColor(.black)
                     Spacer()
                 }
                 
@@ -66,7 +63,9 @@ struct DummyRecipeView: View {
                     ForEach(recipe.ingredients) { ingredient in
                         HStack {
                             
-                            
+                            Image(systemName: "circle.fill")
+                                .foregroundStyle(Color(red: 134/255, green: 191/255, blue: 62/255))
+                                .scaleEffect(0.5)
                             Text(ingredient.name)
                                 .font(.system(size: 16))
                                 .foregroundColor(.black)
@@ -83,7 +82,7 @@ struct DummyRecipeView: View {
                         .foregroundStyle(.clear)
                         .frame(height: 20)
                         .background(Color.white)
-                        Divider().background(Color(red: 134/255, green: 191/255, blue: 62/255, opacity: 1.0))
+
                         
                     }
                     
@@ -96,21 +95,22 @@ struct DummyRecipeView: View {
                         .font(.headline)
                         .fontWeight(.bold)
                         .font(.system(size: 16))
-                        .foregroundColor(.white)
-                        .frame(width: 120, height: 30)
-                        .background(Color(red: 134/255, green: 191/255, blue: 62/255, opacity: 1.0))
-                        .cornerRadius(8)
-                    
-                    
+                        .foregroundColor(.black)
                     Spacer()
                 }
                 
+                Spacer()
+                
                 VStack(alignment: .leading, spacing: 10) {
-                    
-                    
                     ForEach(recipe.instructions) { instruction in
-                        VStack(alignment: .leading){
+                        HStack(alignment: .top){
+                            Text("Step \(instruction.id)")
+                                        .foregroundColor(.primary)
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 14))
                             Text(instruction.step)
+                                .foregroundColor(.primary)
+                                .font(.system(size: 16))
                         }
                         Divider().background(Color(red: 134/255, green: 191/255, blue: 62/255, opacity: 1.0))
                         
@@ -121,8 +121,11 @@ struct DummyRecipeView: View {
         .padding()
     }
 }
-//struct DummyRecipeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        DummyRecipeView(recipe: dummyRecipes[1])
-//    }
-//}
+struct DummyRecipeView_Previews: PreviewProvider {
+    @State static var showingRecipe = false
+    static var previews: some View {
+        
+        DummyRecipeView(recipe: dummyRecipes[1], showingRecipe: $showingRecipe)
+    }
+}
+
